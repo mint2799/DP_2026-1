639a41152f52a19ab8012271bfcde60b903a698a
mint2799 <shk9963@gmail.com>
2026-03-23 04:19:59 +0900

    rename class

--- src/hw/ch02/KoreanOutlet.java
+++ src/hw/ch02/KoreanOutlet.java
@@ -1,6 +1,6 @@
 package hw.ch02;
 
-public class KoreaOutlet {
+public class KoreanOutlet {
     // 한국 표준 전원: 220V AC
     public int provide(){
         System.out.println("한국 콘센트에서 220V AC를 제공합니다.");
