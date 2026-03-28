389ab60511b6d2163b9196539abc135c788fb853
mint2799 <shk9963@gmail.com>
2026-03-28 17:16:29 +0900

    Step 3: Main 클래스수정

--- src/hw/ch02/Main.java
+++ src/hw/ch02/Main.java
@@ -2,6 +2,9 @@ package hw.ch02;
 
 public class Main {
     public static void main(String[] args){
+        System.out.println("학번: 20230776 이름: 김성희");
+        System.out.println();
+        
         KoreanOutlet outlet = new KoreanOutlet();
 
         Charger adapter = new KoreanOutletAdapter(outlet);
