985c794315d3ea3aa4dd6c7d9919cb1fa5ef3a63
mint2799 <shk9963@gmail.com>
2026-03-28 16:58:39 +0900

    Step 3: Main 클래스 작성

---
+++ src/hw/ch02/Main.java
@@ -0,0 +1,13 @@
+package hw.ch02;
+
+public class Main {
+    public static void main(String[] args){
+        KoreanOutlet outlet = new KoreanOutlet();
+
+        Charger adapter = new KoreanOutletAdapter(outlet);
+
+        Smartphone phone = new Smartphone(adapter);
+
+        phone.charge();
+    }
+}
