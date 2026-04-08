18f016b9b665096dab3f39dec8ab797f18985c8e
mint2799 <shk9963@gmail.com>
2026-04-09 00:25:20 +0900

    4월 7일 수업 종료 (01반)

--- src/practice/ch06/Main.java
+++ src/practice/ch06/Main.java
@@ -1,12 +1,27 @@
 package practice.ch06;
 
 import practice.ch06.MessageBox;
+// import prcatice.ch06.UnderlinePen;
 
 public class Main {
     public static void main(String[] args){
+        // 원본(prototype)이 일을 함
         MessageBox mbox1 = new MessageBox('*');
         mbox1.use("Hello, world.");
 
+        // 복제된 객체가 일을 함
         mbox1.createCopy().use("Hello, world.");
+
+        // 원본(prototype)이 일을 함
+        UnderlinePen upen1 = new UnderlinePen('-');
+        upen1.use("Hello, world.");
+
+        // 복제된 객체가 일을 함
+        upen1.createCopy().use("Hello, world.");
+
+        // Manager manager = new Manager();
+        // MessageVox mbox2 = new MessageBox('#');
+
+        // Product p1 = manager.create
     }
 }
