a6acbdd68119642deb46545216617d0362609892
mint2799 <shk9963@gmail.com>
2026-05-05 02:10:49 +0900

    4월 28일 수업 종료 (01반)

--- src/ch12/Sample/Main.java
+++ src/ch12/Sample/Main.java
@@ -2,9 +2,9 @@ package ch12.Sample;
 
 public class Main {
     public static void main(String[] args) {
-        Display b1 = new StringDisplay("Hello, world.");
-        Display b2 = new SideBorder(b1, '#');
-        Display b3 = new FullBorder(b2);
+        Display b1 = new StringDisplay("Hello, world.");   // 중심
+        Display b2 = new SideBorder(b1, '#');   // 장식자1
+        Display b3 = new FullBorder(b2); // 장식자2
         b1.show();
         b2.show();
         b3.show();
