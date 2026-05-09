76b9a7fff0f29d45fd8fbd5a201e8b374468cc87
mint2799 <shk9963@gmail.com>
2026-05-09 16:50:04 +0900

    5월 5일 수업 종료 (01반)

---
+++ src/practice/ch16/Mediator.java
@@ -0,0 +1,9 @@
+package practice.ch16;
+
+public interface Mediator {
+    // Colleague를 생성한다 
+    public abstract void createColleagues();
+
+    // Colleage의 상태가 변화했을 때 호출된다
+    public abstract void colleagueChanged();
+}
