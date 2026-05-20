5d4ae6fe07f3294e299017305853815a21ed1db4
mint2799 <shk9963@gmail.com>
2026-05-20 22:57:38 +0900

    Step 3: Main.java 수정

--- src/hw/ch17/Main.java
+++ src/hw/ch17/Main.java
@@ -2,11 +2,16 @@ package hw.ch17;
 
 public class Main {
     public static void main(String[] args) {
-        NumberGenerator generator = new IncrementalNumberGenerator(10, 50, 5);
+        NumberGenerator generator = new EvenNumberGenerator(0, 30);
+
         Observer observer1 = new DigitObserver();
         Observer observer2 = new GraphObserver();
+        Observer observer3 = new RangeObserver();
+
         generator.addObserver(observer1);
         generator.addObserver(observer2);
+        generator.addObserver(observer3);
+        
         generator.execute();
     }
 }
