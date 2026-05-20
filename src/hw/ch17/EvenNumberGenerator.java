761d8a4c8aaeacbd2f0471b2730a6a7b9e4395b4
mint2799 <shk9963@gmail.com>
2026-05-20 22:53:48 +0900

    Step 2: EvenNumberGenerator 구현

---
+++ src/hw/ch17/EvenNumberGenerator.java
@@ -0,0 +1,25 @@
+package hw.ch17;
+
+public class EvenNumberGenerator extends NumberGenerator {
+    private int start;
+    private int end;
+    private int currNumber;
+
+    public EvenNumberGenerator(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+    
+    @Override
+    public int getNumber() {
+        return currNumber;
+    }
+
+    @Override
+    public void execute() {
+        for (int i = start; i <= end; i += 2) {
+            currNumber = i;
+            notifyObservers();
+        }
+    }
+}
